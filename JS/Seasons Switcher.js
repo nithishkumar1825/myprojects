a01806f88614869439ca470dc5d2e0a79e9016a5
Nithish kumar <nithishkumar18.m@gmail.com>
2023-03-17 12:36:03 +0530

    first commit

---
+++ JS/Seasons Switcher.js
@@ -0,0 +1,32 @@
+let smallImage = document.getElementById("seasonSmallImage")
+let mediumImage = document.getElementById("seasonMediumImage")
+
+let springSmallImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+let springMediumImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+let summerSmallImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+let summerMediumImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+let autumnSmallImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+let autumnMediumImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+let winterSmallImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+let winterMediumImage = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+ 
+
+function springButton() {
+    smallImage.src = springSmallImage;
+    mediumImage.src = springMediumImage;
+}
+
+function summerButton() {
+    smallImage.src = summerSmallImage;
+    mediumImage.src = summerMediumImage;
+}
+
+function autumnButton() {
+    smallImage.src = autumnSmallImage;
+    mediumImage.src = autumnMediumImage;
+}
+
+function winterButton() {
+    smallImage.src = winterSmallImage;
+    mediumImage.src = winterMediumImage;
+}
